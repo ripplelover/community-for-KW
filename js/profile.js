38bd2c87aabd50b3870db43c7a7c7fc8e1144405
BAE-Jungwoo <113417378+BAE-Jungwoo@users.noreply.github.com>
2023-11-24 10:20:14 +0900

    Add files via upload

--- js/profile.js
+++ js/profile.js
@@ -1,12 +1,12 @@
-let userprofileimg = document.getElementById("userprofileimg")
-let usercoverimg = document.getElementById("usercoverimg")
-let progressdiv = document.getElementById("progressdiv")
-let progressbar = document.getElementById("progressbar")
+let userprofileimg = document.getElementById("userprofileimg");
+let usercoverimg = document.getElementById("usercoverimg");
+let progressbar1 = document.getElementById("progressbar");
+let progressbardiv = document.getElementById("progressbardiv");
 
 let fileType = "";
 let uid;
 let updateurl;
-let allUsers = []
+let allUsers = [];
 
 
 let changeCoverImage = (event) => {
@@ -19,16 +19,15 @@ let changeCoverImage = (event) => {
     uploadTask.on('state_changed', 
     (snapshot) => {
         var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        progressbar.style.visibility = "visible"
+        progressbardiv.style.visibility = "visible";
         var uploadpercentage = Math.round(progress);
-        // progressdiv.style.display = "block";
         progressbar.style.width = `${uploadpercentage}%`;
         progressbar.innerHTML = `${uploadpercentage}%`;
     }, 
     (error) => { }, 
     () => {
         uploadTask.snapshot.ref.getDownloadURL().then((coverpicture) => {
-            progressbar.style.visibility = "hidden";
+            progressbardiv.style.visibility = "hidden";
             firebase
             .firestore()
             .collection("users/")
@@ -50,20 +49,20 @@ let changeProfileImage = (event) => {
     uploadTask.on('state_changed', 
     (snapshot) => {
         var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        progressbar.style.visibility = "visible";
+        progressbardiv.style.visibility = "visible";
         var uploadpercentage = Math.round(progress);
-        progressdiv.style.display = "block";
-        progressbar.style.width = `${uploadpercentage}%`;
-        progressbar.innerHTML = `${uploadpercentage}%`;
+        progressbar1.style.width = `${uploadpercentage}%`;
+        progressbar1.innerHTML = `${uploadpercentage}%`;
     }, 
     (error) => { }, 
     () => {
         uploadTask.snapshot.ref.getDownloadURL().then((profileimage) => {
-            progressbar.style.visibility = "hidden"; 
+            progressbardiv.style.visibility = "hidden";
             firebase
             .firestore()
             .collection("users/")
-            .doc(uid).update({ ProfilePicture: profileimage });
+            .doc(uid)
+            .update({ ProfilePicture: profileimage });
         });
     }
     );
@@ -74,26 +73,37 @@ firebase.auth().onAuthStateChanged((user) => {
     if (user) {
         if (user.emailVerified) {
             uid = user.uid;
+
             // Show the image in website
             firebase
             .firestore()
             .collection("users/").
             onSnapshot((result)=>{
-                result.forEach((user)=>{
-                    allUsers.push(user.data())
-                    fileType = user.data().fileType;
-                    if(user.data().uid === user.uid) {
-                        if(user.data().ProfilePicture !== "" || user.data().CoverPicture !== "") {
-                            userprofileimg.setAttribute("src", user.data().ProfilePicture || "https://cdn4.iconfinder.com/data/icons/web-ui-color/128/Account-512.png")
-                            usercoverimg.setAttribute("src", user.data().CoverPicture || "https://c.wallhere.com/photos/3f/04/person_silhouette_bench_evening_decline_sky-741824.jpg!d")
+                result.forEach((users)=>{
+                    allUsers.push(users.data());
+                    fileType = users.data().fileType;
+                    if(users.data().uid === user.uid) {
+                        if(users.data().ProfilePicture !== "" || users.data().CoverPicture !== "") {
+                            userprofileimg.setAttribute("src", users.data().ProfilePicture || "https://cdn4.iconfinder.com/data/icons/web-ui-color/128/Account-512.png")
+                            usercoverimg.setAttribute("src", users.data().CoverPicture || "https://c.wallhere.com/photos/3f/04/person_silhouette_bench_evening_decline_sky-741824.jpg!d")
                         }
                     }
                 });
             });
         } else {
-                window.location.assign("../Pages/emailVerification.html");
+            window.location.assign("../Pages/emailVerification.html");
         }
     } else {
-            window.location.assign("../Pages/Login.html");
+        window.location.assign("../Pages/Login.html");
     }
 });
+
+
+const logout = () => {
+    firebase
+    .auth()
+    .signOut()
+    .then(() => {
+        window.location.assign("../Pages/Login.html");
+    });
+};
